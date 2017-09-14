e157ac9934a6b264f785426708b7743ef598e0bd
Jason Tong <jasontong818@gmail.com>
2017-09-14 16:20:07 -0700

    update

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says I did it', (done) => {
+  chai.request(server)
+    .get('/')
+    .end((err, res) => {
+      expect(err).not.exist;
+      expect(JSON.stringify(res.text)).to.contain('I did it');
+      done();
+    });
+  });
 })
